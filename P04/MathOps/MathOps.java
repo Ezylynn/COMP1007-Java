c5368b6d12c79f4cd59f7aa4e88f11c1dba184d9
Ezylynn <110110711+Ezylynn@users.noreply.github.com>
2025-08-13 10:28:07 +0800

    code pseudocode and java code for assignment week 4

--- P04/MathOps/MathOps.java
+++ P04/MathOps/MathOps.java
@@ -15,132 +15,125 @@ public class MathOps {
     System.out.println("Welcome to PDI Week 3!!!");
 
     while (continueLoop){
-      inputValid = false;
+      try {
+        inputValid = false;
+        System.out.println();
+        System.out.println("What would you like to do?");
+        System.out.println("\t>1. Sum of 2 integers");
+        System.out.println("\t>2. Product of 2 integers");
+        System.out.println("\t>3. Determine if 2 Integers are Divisible");
+        System.out.println("\t>0. Exit");
+        System.out.print("Enter input: ");
 
-      System.out.println();
-      System.out.println("What would you like to do?");
-      System.out.println("\t>1. Sum of 2 integers");
-      System.out.println("\t>2. Product of 2 integers");
-      System.out.println("\t>3. Determine if 2 Integers are Divisible");
-      System.out.println("\t>0. Exit");
-      System.out.print("Enter input: ");
-
-      // check if value input is int 
-      if(sc.hasNextInt()){
         userInput = sc.nextInt(); // take user input in the form of int as options
 
-        // listing cases depending on user selected options
-        switch(userInput){
-          case 1:
-            // output the sum of 2 numbers
-            do{
-              System.out.print("Enter Num 1: ");
-              if (sc.hasNextInt()){
-                num1 = sc.nextInt();
-                System.out.print("Enter Num 2: ");
+          // listing cases depending on user selected options
+          switch(userInput){
+            case 1:
+              // output the sum of 2 numbers
+              do{
+                System.out.print("Enter Num 1: ");
                 if (sc.hasNextInt()){
-                  num2 = sc.nextInt();
-                  numsResult = num1 + num2;
-                  System.out.println("Result: "+ numsResult);
-                  System.out.println("Exit program...");
-                  inputValid = true;
-                  continueLoop = false;
-                  break;
-                }
-                else{
-                  System.out.println("Try again. Has to be an integer");
-                  sc.next();
-                }
-              }
-              else{
-                System.out.println("Try again. Has to be an integer");
-                sc.next();
-              }
-        
-            }
-            while(!inputValid);
-
-
-          case 2:
-            // output product of 2 numbers
-            do{
-              System.out.print("Enter Num 1: ");
-              if (sc.hasNextInt()){
-                num1 = sc.nextInt();
-                System.out.print("Enter Num 2: ");
-                if (sc.hasNextInt()){
-                  num2 = sc.nextInt();
-                  numsResult = num1 * num2;
-                  System.out.println("Result: "+ numsResult);
-                  System.out.println("Exit program...");
-                  inputValid = true;
-                  continueLoop = false;
-                  break;
-                }
-                else{
-                  System.out.println("Try again. Has to be an integer");
-                  sc.next();
-                }
-              }
-              else{
-                System.out.println("Try again. Has to be an integer");
-                sc.next();
-              }
-        
-            }
-            while(!inputValid);
-            break;
-          case 3:
-            // apply modulo to test divisility between 2 numbers
-            do{
-              System.out.print("Enter Num 1: ");
-              if (sc.hasNextInt()){
-                num1 = sc.nextInt();
-                System.out.print("Enter Num 2: ");
-                if (sc.hasNextInt()){
-                  num2 = sc.nextInt();
-                  if (num1%num2==0 || num2%num1==0){
-                    System.out.println(num1 +" & " + num2 + " are divisible");
+                  num1 = sc.nextInt();
+                  System.out.print("Enter Num 2: ");
+                  if (sc.hasNextInt()){
+                    num2 = sc.nextInt();
+                    numsResult = num1 + num2;
+                    System.out.println("Result: "+ numsResult);
+                    inputValid = true;
                   }
                   else{
-                    System.out.println(num1 +" & " + num2 + " are NOT divisible");
+                    System.out.println("Try again. Has to be an integer");
+                    sc.next();
                   }
-                  System.out.println("Exit program...");
-                  inputValid = true;
-                  continueLoop = false;
-                  break;
                 }
                 else{
                   System.out.println("Try again. Has to be an integer");
                   sc.next();
                 }
               }
-              else{
-                System.out.println("Try again. Has to be an integer");
-                sc.next();
-              }
-            }
-            while(!inputValid);
-            break;
-          case 0:
-            // exit program, does nothing if 0
-            System.out.println("Exit program...");
-            continueLoop = false;
-            break;
-          default:
-            // default output if user enter integer beyond 0-3 
-            System.out.println("Integer entered but invalid option, try again");
+              while(!inputValid);
+              break;
+              case 2:
+                // output product of 2 numbers
+                do{
+                  System.out.print("Enter Num 1: ");
+                  if (sc.hasNextInt()){
+                    num1 = sc.nextInt();
+                    System.out.print("Enter Num 2: ");
+                    if (sc.hasNextInt()){
+                      num2 = sc.nextInt();
+                      numsResult = num1 * num2;
+                      System.out.println("Result: "+ numsResult);
+                      inputValid = true;
+                    }
+                    else{
+                      System.out.println("Try again. Has to be an integer");
+                      sc.next();
+                    }
+                  }
+                  else{
+                    System.out.println("Try again. Has to be an integer");
+                    sc.next();
+                  }
             
-        }
-      }
-      else{
-        // stop program if input isn't an integer
-        System.out.println("Invalid output, try again"); 
+                }
+                while(!inputValid);
+                break;
+              case 3:
+                // apply modulo to test divisility between 2 numbers
+                do{
+                  System.out.print("Enter Num 1: ");
+                  if (sc.hasNextInt()){
+                    num1 = sc.nextInt();
+                    System.out.print("Enter Num 2: ");
+                    if (sc.hasNextInt()){
+                      num2 = sc.nextInt();
+                      if (num1%num2==0 || num2%num1==0){
+                        System.out.println(num1 +" & " + num2 + " are divisible");
+                      }
+                      else{
+                        System.out.println(num1 +" & " + num2 + " are NOT divisible");
+                      }
+                      inputValid = true;
+                    }
+                    else{
+                      System.out.println("Try again. Has to be an integer");
+                      sc.next();
+                    }
+                  }
+                  else{
+                    System.out.println("Try again. Has to be an integer");
+                    sc.next();
+                  }
+                }
+                while(!inputValid);
+                break;
+            case 0:
+              // exit program, does nothing if 0
+              System.out.println("Exit program...");
+              continueLoop = false;
+              break;
+            default:
+              // default output if user enter integer beyond 0-3 
+              System.out.println("Integer entered but invalid option, try again");
+          }
+              
+      
+      } catch (InputMismatchException e){ // catch error if input isn't integer
+        System.out.println("Error encountered: " + e);
         sc.next();
+        
       }
-
     }
-
+//        else{
+          // stop program if input isn't an integer
+//          System.out.println("Invalid output, try again"); 
+//          sc.next();
+//        }
     sc.close(); // stop receiving any inputs
+                
+
+
   }
 }
